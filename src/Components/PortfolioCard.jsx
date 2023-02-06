1b4c81bf265eca5d901301df78c2730259cbe388
Augusto Fonte <augustodafonte@gmail.com>
2023-02-06 17:56:03 +0000

    data json

---
+++ src/Components/PortfolioCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+function PortfolioCard() {
+  return (
+    <Link to={`/carlist/${props.numerodechassi}`}>
+    
+    
+  </Link>
+  );
+}
+
+export default PortfolioCard;
--- src/Pages/PortfolioPage.jsx
+++ src/Pages/PortfolioPage.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import MainLayout from '../Layouts/MainLayout';
-
+import data from "../Data/data.json";
 
 
 
 function PortfolioPage() {
   return (
     <MainLayout>
-      <div>
-          Portfolio
-      </div>
+      <div >
+        <ul>
+          {data.map((data) => (
+            <li key={data.titulo}>
+              <CarCard
+                marca={data.titulo}
+                texto={data.texto}
+                github={data.github}
+                linguas={data.linguas}
+                
+              />
+            </li>
+          ))}
+        </ul>
+    </div>
     </MainLayout>
   );
 }
